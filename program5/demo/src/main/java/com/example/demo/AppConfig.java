d185f93e653ceff221f36aa53ca1f75f851c21dc
Anushreel <anushreelgowda12@gmail.com>
2025-01-16 01:17:34 +0530

    n

--- program5/demo/src/main/java/com/example/demo/AppConfig.java
+++ program5/demo/src/main/java/com/example/demo/AppConfig.java
@@ -4,8 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+// @Scope("prototype")
 public class AppConfig {
-    
     @Bean
     public MyService myService() {
         return new MyService();
--- program5/demo/src/main/java/com/example/demo/MyService.java
+++ program5/demo/src/main/java/com/example/demo/MyService.java
@@ -1,6 +1,18 @@
 package com.example.demo;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 
 public class MyService {
+    @PostConstruct
+    public void init() {
+        System.out.println("MyService bean initialized.");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("MyService bean destroyed.");
+    }
+
     public void performTask() {
         System.out.println("Task performed by MyService bean.");
     }
